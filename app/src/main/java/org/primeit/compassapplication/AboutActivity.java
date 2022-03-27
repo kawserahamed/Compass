5c2fb4c693a59d6e910b3b66142cf2c936eb41e8
ahamed404 <kawserarasel24@gmail.com>
2022-03-27 16:26:17 +0600

    All Done

--- app/src/main/java/org/primeit/compassapplication/AboutActivity.java
+++ app/src/main/java/org/primeit/compassapplication/AboutActivity.java
@@ -4,11 +4,26 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.primeit.compassapplication.databinding.ActivityAboutBinding;
+
 public class AboutActivity extends AppCompatActivity {
+
+
+    ActivityAboutBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+        binding = ActivityAboutBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
     }
 }
